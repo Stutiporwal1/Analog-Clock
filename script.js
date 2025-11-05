2e7a6c2af5f555d055a37f0012217e5259997ae2
Stuti Porwal <porwalstuti01@gmail.com>
2025-11-05 15:54:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-const secondhand=document.querySelector('.second-hand');
+const secondhand = document.querySelector('.second-hand');
 const mindhand = document.querySelector('.min-hand');
 const hourhand = document.querySelector('.hour-hand');
 
-function setdate()
-{
+function setdate() {
   const now = new Date();
+
+  // --- Seconds ---
   const seconds = now.getSeconds();
-  const seconddegrees=((seconds/60)*360)+90;
+  const seconddegrees = ((seconds / 60) * 360) + 90;
   secondhand.style.transform = `rotate(${seconddegrees}deg)`;
-  console.log(seconds);
+
+  // --- Minutes (Corrected for smooth movement) ---
   const min = now.getMinutes();
-  const mindegrees=((min/60)*360)+90;
+  // We add the seconds' fraction to the minute for a smoother tick
+  const mindegrees = (((min + (seconds / 60)) / 60) * 360) + 90;
   mindhand.style.transform = `rotate(${mindegrees}deg)`;
 
+  // --- Hours (Corrected for 12-hr format and smooth movement) ---
   const hour = now.getHours();
-  const hourdegrees=((hour/12)*360)+90;
+  // We use (hour % 12) for the 12-hour format
+  // We add the minutes' fraction to the hour for smooth movement
+  const hourdegrees = ((((hour % 12) + (min / 60)) / 12) * 360) + 90;
   hourhand.style.transform = `rotate(${hourdegrees}deg)`;
-  
 }
-setInterval(setdate,1000);
+
+setInterval(setdate, 1000);
+setdate(); 
